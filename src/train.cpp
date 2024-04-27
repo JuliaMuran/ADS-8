d5c0fbe0ea907f519aa4a49f6629c1d4cedafd46
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-04-27 15:45:12 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -2,58 +2,60 @@
 #include "train.h"
 #include <cstddef>
 
-void Train::addCage(bool light) {
-        Cage* node = new Cage;
-        node->light = light;
-        if (first == NULL) {
-            first = node;
-            first->prev = node;
-            first->next = node;
-        } else {
-            node->prev = first->prev;
-            node->next = first;
-            first->prev->next = node;
-            first->prev = node;
-        }
-}
-int Train::getLength() {
-    Cage* current = first;
-    if (first->light == false) {
-        first->light = true;
-    }
-    int len = 0;
-    int numberV = 1;
-    while (true) {
-        countOp++;
-        current = current->next;
-        len++;
-        numberV++;
-        int index;
-        if (current->light == true) {
-            current->light = false;
-            index = numberV;
-            while (index > 1) {
-                current = current->prev;
-                countOp++;
-                index--;
-            }
-            if (current->light == false) {
-                break;
+Train::Train() {
+    void Train::addCage(bool light) {
+            Cage* node = new Cage;
+            node->light = light;
+            if (first == NULL) {
+                first = node;
+                first->prev = node;
+                first->next = node;
             } else {
-                index = 1;
-                while (index < numberV) {
+                node->prev = first->prev;
+                node->next = first;
+                first->prev->next = node;
+                first->prev = node;
+            }
+    }
+    int Train::getLength() {
+        Cage* current = first;
+        if (first->light == false) {
+            first->light = true;
+        }
+        int len = 0;
+        int numberV = 1;
+        while (true) {
+            countOp++;
+            current = current->next;
+            len++;
+            numberV++;
+            int index;
+            if (current->light == true) {
+                current->light = false;
+                index = numberV;
+                while (index > 1) {
                     current = current->prev;
                     countOp++;
-                    index++;
+                    index--;
                 }
+                if (current->light == false) {
+                    break;
+                } else {
+                    index = 1;
+                    while (index < numberV) {
+                        current = current->prev;
+                        countOp++;
+                        index++;
+                    }
+                    current->light = true;
+                }
+            } else {
                 current->light = true;
             }
-        } else {
-            current->light = true;
         }
+        return len;
     }
-    return len;
-}
-int Train::getOpCount() {
-    return countOp;
+    int Train::getOpCount() {
+        return countOp;
+    }
 }
